996910ec09e8b7c547fce32eb91bf8af2b136fc2
harshtarsariya0106 <harshtarsariya@gamil.com>
2026-01-17 11:34:48 +0530

    update data

--- Cartify/shop/admin.py
+++ Cartify/shop/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from . models import product, Contact,Orders
+from . models import Category, product, Contact,Orders
 
 # Register your models here.
 admin.site.register(product),
 admin.site.register(Contact),
-admin.site.register(Orders)
+admin.site.register(Orders),
+admin.site.register(Category)
--- Cartify/shop/models.py
+++ Cartify/shop/models.py
@@ -3,11 +3,14 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 # Create your models here.
 class product(models.Model):
     product_id = models.AutoField(primary_key=True  )
     product_name = models.CharField(max_length=50)
-    category = models.CharField(max_length=50, default="")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     subcategory = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
     desc = models.TextField(max_length=300)
